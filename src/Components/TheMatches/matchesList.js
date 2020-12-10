2c3e045beca070e3985b8175420e9cf1377d4aa8
Olayiwola Olanrewaju <larry_coal@outlook.com>
2020-12-10 23:54:47 +0100

    matches

--- src/Components/TheMatches/matchesList.js
+++ src/Components/TheMatches/matchesList.js
@@ -6,13 +6,15 @@ const MatchesList = (props) => {
   const [state, setState] = useState({});
 
   useEffect(() => {
+      console.log(props.matches)
     setState({
-      matches: props.matches,
+      matches: props.matches
     });
-  }, [props.matches]);
+  },[props]);
 
-  const showMatches = () =>
-    state.matches
+  const showMatches = () =>{
+  console.log(state.matches)
+  return  state.matches
       ? state.matches.map((match, i) => (
           <NodeGroup
             data={state.matches}
@@ -46,7 +48,27 @@ const MatchesList = (props) => {
                       opacity,
                       transform:`translate(${x}px)`
                   }}>
-hello
+                      <div className="block_wraper">
+                          <div className="block">
+                              <div className="icon" style={{
+                                  background:`url(/images/team_icons/${data.localThmb}.png)`
+                              }}></div>
+                              <div className="team">{data.local}</div>
+                            <div className="result">{data.resultLocal}</div>
+                          </div>
+                          <div className="block">
+                              <div className="icon" style={{
+                                  background:`url(/images/team_icons/${data.awayThmb}.png)`
+                              }}></div>
+                              <div className="team">{data.away}</div>
+                            <div className="result">{data.resultAway}</div>
+                          </div>
+                      </div>
+                      <div className="block_wraper nfo">
+                            <div><strong>Date :</strong>{data.date}</div>
+                            <div><strong>Stadium:</strong>{data.stadium}</div>
+                            <div><strong>Referee:</strong>{data.referee}</div>
+                      </div>
                   </div>
                 ))}
               </div>
@@ -54,7 +76,7 @@ hello
           </NodeGroup>
         ))
       : null;
-
+        }
   return (
       <div>
           {showMatches()}
